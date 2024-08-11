6641c997c1c012d000527838a88e740bc8908f0e
Chandra Halim <chan.halim98@gmail.com>
2024-08-12 01:01:42 +0700

    f

--- public/js/main.js
+++ public/js/main.js
@@ -1,67 +1,3 @@
-// export const element = () => {
-//   const opacity = (id, speed = 0.01) => {
-//     const element = document.getElementById(id);
-//     let op = parseInt(element.style.opacity);
-
-//     let clear = null;
-//     clear = setInterval(() => {
-//       if (op > 0) {
-//         element.style.opacity = op.toString();
-//         op -= speed;
-//       } else {
-//         clearInterval(clear);
-//         clear = null;
-//         element.remove();
-//       }
-//     }, 10);
-//   };
-
-//   const open = async (button) => {
-//     button.disabled = true;
-//     confetti({
-//       origin: { y: 1 },
-//       zIndex: 1057,
-//     });
-
-//     document.querySelector("body").style.overflowY = "scroll";
-//     if (storage("information").get("info")) {
-//       document.getElementById("information")?.remove();
-//     }
-
-//     const token = document.querySelector("body").getAttribute("data-key");
-//     if (!token || token.length === 0) {
-//       document.getElementById("ucapan")?.remove();
-//       document
-//         .querySelector('a.nav-link[href="#ucapan"]')
-//         ?.closest("li.nav-item")
-//         ?.remove();
-//     }
-
-//     AOS.init();
-
-//     countDownDate();
-//     opacity("welcome", 0.025);
-
-//     audio.play();
-//     audio.showButton();
-
-//     theme.check();
-//     theme.showButtonChangeTheme();
-
-//     if (!token || token.length === 0) {
-//       return;
-//     }
-
-//     const status = await storeConfig(token);
-//     if (status === 200) {
-//       animation();
-//       await comment.comment();
-//     }
-//   };
-
-//   return { opacity, open };
-// };
-
 document.getElementById("open").addEventListener("click", function () {
   var container = document.getElementById("openContainer");
   var halo = document.getElementById("halo");
@@ -74,3 +10,34 @@ document.getElementById("open").addEventListener("click", function () {
     halo.style.display = "block";
   });
 });
+
+function initMap() {
+  var location = { lat: -34.397, lng: 150.644 };
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 15,
+    center: location,
+  });
+  var marker = new google.maps.Marker({
+    position: location,
+    map: map,
+  });
+}
+
+var countDownDate = new Date("Dec 25, 2024 10:00:00").getTime();
+var x = setInterval(function () {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("countdown").innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "EXPIRED";
+  }
+}, 1000);
